cd53902894f229349ee935990dc70dbd4c60546a
Ben Eggers <beggerss@cs.washington.edu>
2014-06-11 17:15:39 -0700

    bubble sort

--- bubble.go
+++ bubble.go
@@ -5,3 +5,19 @@ package sorts
 
 // Bubble sort
 
+import (
+	"sort"
+)
+
+func bubbleSort(data sort.Interface) {
+	keepGoing := true
+	for !keepGoing {
+		keepGoing = false
+		for i := 0; i < data.Len() - 1; i++ {
+			if data.Less(i, i+1) {
+				data.Swap(i, i+1)
+				keepGoing = true
+			}
+		}
+	}
+}
